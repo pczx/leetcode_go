ae9c6e52d3c37749880712039e0201c2335c5097
czp <965227896@qq.com>
2021-01-07 23:05:35 +0800

    leetcode_go

---
+++ solution34/solution34.go
@@ -0,0 +1,46 @@
+package solution34
+
+// Definition for a binary tree node.
+type TreeNode struct {
+	Val   int
+	Left  *TreeNode
+	Right *TreeNode
+}
+
+func pathSum(root *TreeNode, sum int) [][]int {
+	paths := [][]int{}
+
+	path := []int{}
+
+	var dfs func(*TreeNode, int)
+
+	dfs = func(root *TreeNode, currSum int) {
+		if root == nil {
+			return
+		}
+
+		currSum += root.Val
+
+		path = append(path, root.Val)
+
+		if root.Left == nil && root.Right == nil && currSum == sum {
+			temp := make([]int, len(path))
+			copy(temp, path)
+			paths = append(paths, temp)
+		}
+
+		if root.Left != nil {
+			dfs(root.Left, currSum)
+		}
+
+		if root.Right != nil {
+			dfs(root.Right, currSum)
+		}
+
+		path = path[:len(path)-1]
+	}
+
+	dfs(root, 0)
+
+	return paths
+}
